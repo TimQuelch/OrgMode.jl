ea693e3aa5c2722f659d1fbb3b2b705bd63bbbf4
Documenter.jl <documenter@juliadocs.github.io>
2020-10-02 07:46:54 +0000

    build based on 06ae7fe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OrgMode","category":"page"},{"location":"#OrgMode","page":"Home","title":"OrgMode","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OrgMode]","category":"page"}]
+}
