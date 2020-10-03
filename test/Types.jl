0132166ac6c091f27b8345c5bde1c366cf183f0b
Tim Quelch <tim@tquelch.com>
2020-10-03 10:23:50 +1000

    Move tests into testsets

--- test/Types.jl
+++ test/Types.jl
@@ -5,36 +5,40 @@ using OrgMode: GreaterElement, Element, Environment, Headline, Section
 end
 
 @testset "Headline" begin
-    @test Headline <: GreaterElement
+    @testset "Type" begin
+        @test Headline <: GreaterElement
 
-    for f in (:level, :title, :todo, :tags, :priority, :section, :headlines)
-        @test hasfield(Headline, f)
+        for f in (:level, :title, :todo, :tags, :priority, :section, :headlines)
+            @test hasfield(Headline, f)
+        end
     end
 
-    h = Headline(level=1)
-    @test h.level === 1
-    @test h.title === nothing
-    @test h.todo === nothing
-    @test h.tags == []
-    @test h.priority === nothing
-    @test h.section === nothing
-    @test h.headlines == []
+    @testset "Constructors" begin
+        h = Headline(level=1)
+        @test h.level === 1
+        @test h.title === nothing
+        @test h.todo === nothing
+        @test h.tags == []
+        @test h.priority === nothing
+        @test h.section === nothing
+        @test h.headlines == []
 
-    @test_throws DomainError Headline(level=-1)
+        @test_throws DomainError Headline(level=-1)
 
-    h = Headline(
-        level=4,
-        title="Headline title",
-        todo="TODO",
-        tags=["email", "noexport"],
-        priority='A',
-        section=Section([]),
-        headlines=[Headline(level=1), Headline(level=2)])
-    @test h.level == 4
-    @test h.title == "Headline title"
-    @test h.todo == "TODO"
-    @test h.tags == ["email", "noexport"]
-    @test h.priority == 'A'
-    @test h.section isa Section
-    @test h.headlines isa AbstractArray{Headline,1}
+        h = Headline(
+            level=4,
+            title="Headline title",
+            todo="TODO",
+            tags=["email", "noexport"],
+            priority='A',
+            section=Section([]),
+            headlines=[Headline(level=1), Headline(level=2)])
+        @test h.level == 4
+        @test h.title == "Headline title"
+        @test h.todo == "TODO"
+        @test h.tags == ["email", "noexport"]
+        @test h.priority == 'A'
+        @test h.section isa Section
+        @test h.headlines isa AbstractArray{Headline,1}
+    end
 end
