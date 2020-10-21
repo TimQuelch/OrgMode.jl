2608016c9692af84c5dbf635e46b17df450650bb
Tim Quelch <tim@tquelch.com>
2020-10-21 11:59:39 +1100

    Add map function as first processing function

---
+++ test/Process.jl
@@ -0,0 +1,26 @@
+using OrgMode
+using OrgMode: map, parse
+using OrgMode.Types
+
+@testset "map" begin
+    basic = """
+        This is a basic org document
+        with some multiline paragraphs
+
+        * And some headings
+        These headings contain text
+        ** And nested headings
+        That also contain text
+        ** And another that doesn't
+        * Finally we go back to level one
+        and close off with some text
+
+        but this text is split into two paragraphs
+        """
+
+    d = parse(basic)
+    @test map(typeof, Headline, d) == [Headline, Headline, Headline, Headline]
+    @test map(typeof, Section, d) == [Section, Section, Section, Section]
+    @test map(typeof, [Headline, Section], d) == [Section, Headline, Section, Headline,
+                                                  Section, Headline, Headline, Section]
+end
