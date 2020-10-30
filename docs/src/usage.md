098e80faeca4c2612239c18a8ce797152c1f36a5
Tim Quelch <tim@tquelch.com>
2020-10-30 17:52:50 +1100

    More docs

--- docs/src/usage.md
+++ docs/src/usage.md
@@ -1,5 +1,12 @@
 # Usage
 
+```@repl
+using OrgMode
+s = "A paragraph\n\n* Headline 1n\nSection content\n** Headline 1.1\nAnd more content"
+d = OrgMode.parse(s)
+OrgMode.children(d)
+```
+
 ```@docs
 OrgMode.parse
 OrgMode.map
--- src/OrgMode.jl
+++ src/OrgMode.jl
@@ -1,5 +1,11 @@
 """
-The main public interface
+Provides functions to parse and process emacs Org Mode files.
+
+The syntax tree of an org file can be read with [`parse`](@ref). This results in a tree-like
+structure that can be accessed directly or processed with [`map`](@ref).
+
+Heavily inspired by the Emacs lisp native parser
+[`org-element`](https://orgmode.org/worg/dev/org-element-api.html)
 """
 module OrgMode
 
